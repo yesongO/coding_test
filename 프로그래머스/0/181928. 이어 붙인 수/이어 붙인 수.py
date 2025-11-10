cc8ad1285eaf9d92c72da0d3120cf26363b7dc65
oh ye-song <yesong9874@naver.com>
2025-11-11 08:55:12 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.03 ms, Memory: 9.41 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    odd_num = ''
+    even_num = ''
+    
+    for i in num_list:
+        if i % 2 != 0:
+            odd_num += str(i)
+        else:
+            even_num += str(i)
+            
+    return int(odd_num) + int(even_num)
