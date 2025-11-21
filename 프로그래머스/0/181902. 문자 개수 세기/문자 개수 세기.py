df12603cc98a132789f2e2951f62cd5404fbb025
oh ye-song <yesong9874@naver.com>
2025-11-21 23:59:33 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.74 ms, Memory: 9.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -0,0 +1,7 @@
+def solution(my_string):
+    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    answer = [0] * 52
+    for s in my_string:
+        idx = alphabet.index(s)
+        answer[idx] += 1
+    return answer
