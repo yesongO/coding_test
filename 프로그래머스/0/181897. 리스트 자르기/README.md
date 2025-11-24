a070ff682239120fb440fad23decd50cf297895e
oh ye-song <yesong9874@naver.com>
2025-11-24 17:54:24 +0900

    [level 0] Title: 리스트 자르기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/README.md
+++ 프로그래머스/0/181897. 리스트 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 리스트 자르기 - 181897 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181897) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181897?language=javascript) 
 
 ### 성능 요약
 
-메모리: 9.22 MB, 시간: 0.00 ms
+메모리: 33.4 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 23일 12:56:23
+2025년 11월 24일 17:54:20
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,16 @@
+function solution(n, slicer, num_list) {
+    const [a, b, c] = slicer;
+    if (n === 1) {
+        return num_list.slice(0, b+1);
+    } else if (n === 2) {
+        return num_list.slice(a);
+    } else if (n === 3) {
+        return num_list.slice(a, b+1)
+    } else {
+        const result = []
+        for (let i=a; i<=b; i=i+c) {
+            result.push(num_list[i]);
+        }
+        return result;
+    } 
+}
