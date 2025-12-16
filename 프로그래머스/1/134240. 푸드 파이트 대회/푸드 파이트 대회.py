4056e5978f63f7234d6c425c6ed0b99e8cf8adae
oh ye-song <yesong9874@naver.com>
2025-12-16 23:49:27 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.02 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,12 +1,7 @@
 def solution(food):
     one_side = ""
     for i in range(1, len(food)):
-        if food[i] >= 2 and food[i] % 2 != 0:
-            add_side_num = (food[i] - 1) // 2
-            one_side += str(i) * add_side_num
-        elif food[i] >= 2 and food[i] % 2 == 0:
-            add_side_num = food[i] // 2
-            one_side += str(i) * add_side_num
+        one_side += str(i) * (food[i] // 2)
     return one_side + "0" + one_side[::-1]
             
         
