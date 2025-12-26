cb9ef21a8002f20c85dc878f196cb68372985628
oh ye-song <yesong9874@naver.com>
2025-12-26 19:32:33 +0900

    [level 1] Title: 약수의 합, Time: 0.69 ms, Memory: 9.28 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -0,0 +1,9 @@
+def solution(n):
+    result = 0
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            result += i
+            if i != n // i:
+                result += n // i
+    return result
+            
