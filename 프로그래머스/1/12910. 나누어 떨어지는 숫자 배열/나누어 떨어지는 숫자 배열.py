32dd9f25d53a24c35c6363654633d34a21795b2c
oh ye-song <yesong9874@naver.com>
2026-01-01 19:07:08 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 3.49 ms, Memory: 12.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.py
@@ -0,0 +1,7 @@
+def solution(arr, divisor):
+    result = []
+    for i in arr:
+        if i % divisor == 0:
+            result.append(i)
+    if len(result) == 0: return [-1]
+    return sorted(result)
