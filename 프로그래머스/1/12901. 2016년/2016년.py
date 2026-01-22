d4dc38dc98f0137aa094e6a3878cb66d498398bf
oh ye-song <yesong9874@naver.com>
2026-01-23 08:47:03 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -0,0 +1,11 @@
+def solution(a, b):
+    month_day_lst = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    week_day_lst = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED']
+    
+    day_sum = 0
+    for i in range(a-1):
+        day_sum += month_day_lst[i]
+    day_sum += b
+    
+    return week_day_lst[day_sum % 7]
+
