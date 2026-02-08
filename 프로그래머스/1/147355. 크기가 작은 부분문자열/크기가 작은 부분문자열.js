309d0fecb6b935e08d09599aef44af9c1a9fec6d
oh ye-song <yesong9874@naver.com>
2026-02-08 18:51:37 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 27.96 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,9 +1,16 @@
 function solution(t, p) {
-    let com_num = [];
-    let com_len = t.length - p.length + 1;
-    for(let i=0; i<com_len; i++) {
-        com_num.push(Number(t.slice(i, i+p.length)));
+    const p_len = p.length;
+    const split_num_lst = [];
+    let result = 0;
+    
+    for (let i=0; i<t.length - p_len + 1; i++) {
+        split_num_lst.push(Number(t.slice(i, i+p_len)));
     }
-    let filtered = com_num.filter(i => i <= Number(p));
-    return filtered.length;
+    
+    split_num_lst.forEach((num) => {
+        if (num <= Number(p)) {
+            result += 1;
+        }
+    });
+    return result;
 }
