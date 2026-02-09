87cb98ce51e57dda2e5b79fe8182b00b83e31823
oh ye-song <yesong9874@naver.com>
2026-02-09 15:28:38 +0900

    [level 1] Title: 문자열 나누기, Time: 5.28 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,20 +1,20 @@
 from collections import deque
 
 def solution(s):
-    my_queue = deque([str for str in s])
-    result = 0 
+    my_queue = deque(s)
+    result = 0
     
     while my_queue:
-        first_str = my_queue[0]  
+        first_str = my_queue[0]
         first_str_count = 1
         another_str_count = 0
         
-        for i in range(1, len(my_queue)): 
-            if my_queue[i] == first_str:
+        for i in range(1, len(my_queue)):
+            if first_str == my_queue[i]:
                 first_str_count += 1
             else:
-                another_str_count += 1 
-            
+                another_str_count += 1
+
             if first_str_count == another_str_count:
                 for j in range(first_str_count * 2):
                     my_queue.popleft()
@@ -23,10 +23,15 @@ def solution(s):
         else:
             result += 1
             break
-                
+            
     return result
+            
         
         
+                                
+        
+        
+    
             
         
         
