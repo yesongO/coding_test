f6f63ccdeac79bc87435e9c2f5b06b83d93bd8dd
oh ye-song <yesong9874@naver.com>
2026-02-18 23:30:25 +0900

    [level 1] Title: 숫자 짝꿍, Time: 343.14 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/README.md
+++ 프로그래머스/1/131128. 숫자 짝꿍/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 짝꿍 - 131128 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131128) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131128?language=python3) 
 
 ### 성능 요약
 
-메모리: 26.2 MB, 시간: 357.31 ms
+메모리: 26.3 MB, 시간: 343.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 19일 08:46:13
+2026년 02월 18일 23:30:22
 
 ### 문제 설명
 
--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,13 +1,13 @@
 def solution(X, Y):
-    all_num = '9876543210'
+    num_lst = '9876543210'
     result = ''
-    for num in all_num: # 이때 num은 문자열 ex) '9'
-        min_count = min(X.count(num), Y.count(num))
-        for i in range(min_count):
-            result += num
     
+    for i in num_lst:
+        min_i_count = min(X.count(i), Y.count(i))
+        for j in range(min_i_count):
+            result += i
+            
     if len(result) == 0: return '-1'
     if result[0] == '0': return '0'
-    
-    return result
 
+    return result
